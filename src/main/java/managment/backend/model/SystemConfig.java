35df040deaeb543ec52f327d9e787953e28c8848
KeshUdana <125798571+KeshUdana@users.noreply.github.com>
2024-11-06 13:59:01 +0530

    CLI done

--- src/main/java/managment/backend/model/SystemConfig.java
+++ src/main/java/managment/backend/model/SystemConfig.java
@@ -4,38 +4,43 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SystemConfig {
-    private int totalTickets; //Can change
-    private int vendorReleaseRate;
-    private int userRetrievalRate;
+    private int totalTickets; // Can change
+    private double vendorReleaseRate;
+    private double userRetrievalRate;
     private int maxTicketCapacity;
-    private int maxThreads;//Not sure about this bit ,but keeping for later
+    private int maxThreads; // Not sure about this bit, but keeping for later
 
-    //Getters
-    public int getMaxTicketCapacity(){ //No setter assigned as this is immutable in this system
+    // Getters
+    public int getMaxTicketCapacity() { // No setter assigned as this is immutable in this system
         return maxTicketCapacity;
     }
 
-    public int getTotalTicket() {
+    public int getTotalTickets() {
         return totalTickets;
     }
 
-    public int getVendorReleaseRate(){
+    public double getVendorReleaseRate() {
         return vendorReleaseRate;
     }
-    public int getUserRetrievalRate(){
+
+    public double getUserRetrievalRate() {
         return userRetrievalRate;
     }
 
-    //Setters
-    public void setTotalTickets(){
-        this.totalTickets=totalTickets; //Subject to change under Vendors
-    }
-    public void setVendorReleaseRate(){
-        this.vendorReleaseRate=vendorReleaseRate;
-    }
-    public void setUserRetrievalRate(){
-        this.userRetrievalRate=userRetrievalRate;//Subject to change by Vendors as well
+    // Setters
+    public void setMaxTicketCapacity(int maxTicketCapacity) { // This should be immutable later
+        this.maxTicketCapacity = maxTicketCapacity;
     }
 
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets; // Subject to change under Vendors
+    }
 
+    public void setVendorReleaseRate(double vendorReleaseRate) {
+        this.vendorReleaseRate = vendorReleaseRate;
+    }
+
+    public void setUserRetrievalRate(double userRetrievalRate) {
+        this.userRetrievalRate = userRetrievalRate; // Subject to change by Vendors as well
+    }
 }
