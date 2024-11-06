6f069d7ff2c9fb2db59705a57674f06168f07182
KeshUdana <125798571+KeshUdana@users.noreply.github.com>
2024-11-06 11:43:06 +0530

    CLI

--- src/main/java/managment/backend/model/SystemConfig.java
+++ src/main/java/managment/backend/model/SystemConfig.java
@@ -8,5 +8,34 @@ public class SystemConfig {
     private int vendorReleaseRate;
     private int userRetrievalRate;
     private int maxTicketCapacity;
-    private int maxThreads;
+    private int maxThreads;//Not sure about this bit ,but keeping for later
+
+    //Getters
+    public int getMaxTicketCapacity(){ //No setter assigned as this is immutable in this system
+        return maxTicketCapacity;
+    }
+
+    public int getTotalTicket() {
+        return totalTickets;
+    }
+
+    public int getVendorReleaseRate(){
+        return vendorReleaseRate;
+    }
+    public int getUserRetrievalRate(){
+        return userRetrievalRate;
+    }
+
+    //Setters
+    public void setTotalTickets(){
+        this.totalTickets=totalTickets; //Subject to change under Vendors
+    }
+    public void setVendorReleaseRate(){
+        this.vendorReleaseRate=vendorReleaseRate;
+    }
+    public void setUserRetrievalRate(){
+        this.userRetrievalRate=userRetrievalRate;//Subject to change by Vendors as well
+    }
+
+
 }
