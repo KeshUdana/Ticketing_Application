348cb0b8de6ec1cf0d42822cff05ee6a66aad368
KeshUdana <125798571+KeshUdana@users.noreply.github.com>
2024-11-07 20:44:53 +0530

    adding serialization

--- src/main/java/Startup/ConfigManager.java
+++ src/main/java/Startup/ConfigManager.java
@@ -1,9 +1,31 @@
 package Startup;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 public class ConfigManager {
+    private static final String CONFIG_FILE="config.json";
+    private static final Gson gson=new GsonBuilder().setPrettyPrinting().create();
 
+    //Saving to JSON file
+    public static void saveConfig(SystemConfig config){
+        try(FileWriter writer=new FileWriter(CONFIG_FILE)){
+            gson.toJson(config,writer);
+            System.out.println("Configuration Saved");
+        }catch(IOException e){
+            System.out.println("Error "+e.getMessage());
+        }
+    }
+    //Load from JSON
+
+    public static SystemConfig loadConfig(){
+        try(FileReader reader=new FileReader(CONFIG_FILE)){
+            return gson.fromJson(reader,SystemConfig.class);
+        }catch (IOException e){
+            System.out.println("Error loading "+e.getMessage());
+            return new SystemConfig();
+        }
+    }
 }
