8d14a13bb93f8af6ac079891fdc8df65c9571234
KeshUdana <125798571+KeshUdana@users.noreply.github.com>
2024-12-09 13:02:33 +0530

    chore(backend): remove Spring Security to allow API access
    Attempting to remove Spring Security as the API now handles backend security. Spring Security restrictions are currently blocking access to endpoints.

--- backend/src/main/java/managment/backend/BackendApplication.java
+++ backend/src/main/java/managment/backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.io.File;
@@ -12,7 +13,7 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, SecurityAutoConfiguration.class})
 @ComponentScan(basePackages = "Startup")
 public class BackendApplication {
 
--- backend/src/main/java/managment/backend/controller/TicketController.java
+++ backend/src/main/java/managment/backend/controller/TicketController.java
@@ -1,7 +1,6 @@
 package managment.backend.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,3 +33,5 @@ public class TicketController {
         }
     }
 }
+
+
